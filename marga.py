210fa39882e3a92d9b47cc84a329782cd4e407ae
margaaga <margagaga@gmail.com>
2024-09-28 11:16:02 +0800

    HAHAHAA

---
+++ marga.py
@@ -0,0 +1,18 @@
+age = ["-12,13,-14,15,-16"]
+username = ["Marga, cute, kaayo, HAHAHA, duper, duper"]
+
+#for loop
+for dataage in age:
+    if dataage < 0: 
+        print(str(dataage) + " negative")
+    elif dataage > 0:
+        print(str(dataage) + " positive")
+    
+#for loop
+
+for datausername in username:
+    if username == "Marga":
+        print("marga cotie")
+    else:
+        print("marga not cotie")
+    
